a33b3385c3be515e0991ae5f1ee959a5b759928a
Mikkel Andersen <dasmikko@gmail.com>
2017-06-19 14:24:14 +0200

    Added persitant redux store

--- src/js/store.js
+++ src/js/store.js
@@ -2,9 +2,18 @@ import { applyMiddleware, createStore } from "redux";
 import { reducers } from "./reducers/Reducer";
 import ReduxThunk from 'redux-thunk'
 
+// Persistant store
+const persistedState = localStorage.getItem('reduxState') ? JSON.parse(localStorage.getItem('reduxState')) : {}
 
 const middleware = applyMiddleware(ReduxThunk);
 
 export default () => {
-    return createStore(reducers, middleware);
+    let store = createStore(reducers, persistedState, middleware);
+
+    // Update the localStorage on each dispatch
+    store.subscribe(() => {
+        localStorage.setItem('reduxState', JSON.stringify(store.getState()))
+    });
+
+    return store;
 };
